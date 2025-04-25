1b16f0ec92571f57dcefaccf89e08198307bb529
Darrien Rushing <drushing.dev@gmail.com>
2025-04-25 01:28:22 -0500

    can read a csv

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ from supabase import create_client, Client
 import os
 from dotenv import load_dotenv
 from rich import print
+import csv
 
 load_dotenv()
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+
+def read_csv():
+    file_path = 'ingest_data.csv'
+    
+    with open(file_path, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        
+        for row in reader:
+            print(row)
 
 def main():
     # Initialize client
