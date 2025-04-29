f3d6a67ad5c007d64873632ee1e4ed635d59be0b
Darrien Rushing <drushing.dev@gmail.com>
2025-04-29 00:06:36 -0500

    Read CSV, directed by CLI flag

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-from supabase import create_client, Client
 import os
+import sys
+import csv
+import argparse
 from dotenv import load_dotenv
 from rich import print
-import csv
+from supabase import create_client, Client
 
 load_dotenv()
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
-def read_csv():
-    file_path = 'ingest_data.csv'
+def read_csv(file_name: str):
+    if '.csv' in file_name:
+        with open(file_name, mode='r', newline='') as file:
+            reader = csv.reader(file)
+            
+            for row in reader:
+                print(row)
     
-    with open(file_path, mode='r', newline='') as file:
-        reader = csv.reader(file)
-        
-        for row in reader:
-            print(row)
+    print('Error: File type must be .csv')
+    sys.exit(1)
 
-def main():
+def insert_row():
     # Initialize client
     supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -33,6 +37,15 @@ def main():
 
     # Check the response
     print(f"[bold green]{response}[/bold green]")
+
+def main():
+    parser = argparse.ArgumentParser(description="Python Supabase Loader Utility")
+    parser.add_argument("-f", "--file", type=str, help = "File to import. MUST BE CSV.")
+    args = parser.parse_args()
+    
+    if args.file:
+        file_name = args.file
+        read_csv(file_name)
 
 if __name__ == "__main__":
     main()
